cb0f2cd65bfcc074d12a6f4c1ae51ca73fa2a7b1
Mauro Sabatino <mauro.sabatino@intesasanpaolo.com>
2018-02-24 14:17:11 +0100

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Mauro Sabatino  on 24/02/2018.
+//  Copyright © 2018 Mauro Sabatino . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
